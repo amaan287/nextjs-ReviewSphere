178ccd388a8267dcf706d55bbb76c33f7cad4c30
amaan287 <amaanmirza287@gmail.com>
2024-12-18 22:35:02 +0530

    setting up homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,8 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <div className="flex flex-col items-center justify-center w-full max-w-[600px] gap-4">
-        <h1 className="text-4xl font-bold text-center">Welcome to Geist UI</h1>
-        <p className="text-lg text-center">
-          Geist UI is a modern and minimalist design system for React. It is a
-          set of React components that are highly customizable and easy to use.
-        </p>
+        <h1 className="text-4xl font-bold text-center">Welcome to homepage</h1>
+        <p className="text-lg text-center">Login to post a review</p>
       </div>
     </div>
   );
