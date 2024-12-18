a8470eebf931825cec5423cdd665fb892a0af695
amaan287 <amaanmirza287@gmail.com>
2024-12-18 22:41:49 +0530

    making basic folder structure

---
+++ src/app/signin/page.tsx
@@ -0,0 +1,12 @@
+export default function Signin() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <div className="flex flex-col items-center justify-center w-full max-w-[600px] gap-4">
+        <h1 className="text-4xl font-bold text-center">Sign up</h1>
+        <p className="text-lg text-center">
+          Create an account to post a review
+        </p>
+      </div>
+    </div>
+  );
+}
